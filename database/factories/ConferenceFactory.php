40472e3e48f1442a2bd20945ff2a1108b690aa34
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-03 21:34:59 +0300

    Code refactor

--- database/factories/ConferenceFactory.php
+++ database/factories/ConferenceFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Enums\Region;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 use App\Models\Conference;
@@ -27,7 +28,7 @@ class ConferenceFactory extends Factory
             'start_date' => $this->faker->dateTime(),
             'end_date' => $this->faker->dateTime(),
             'status' => $this->faker->word(),
-            'region' => $this->faker->word(),
+            'region' => $this->faker->randomElement(Region::class),
             'venue_id' => Venue::factory(),
         ];
     }
