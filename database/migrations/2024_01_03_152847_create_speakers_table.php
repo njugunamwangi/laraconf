4896ff45cdec75dcade3a8e9f4eeede9fb96d56b
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-03 23:55:53 +0300

    Add qualifications to speakers

--- database/migrations/2024_01_03_152847_create_speakers_table.php
+++ database/migrations/2024_01_03_152847_create_speakers_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email');
             $table->text('bio');
+            $table->json('qualifications');
             $table->string('twitter_handle');
             $table->timestamps();
         });
