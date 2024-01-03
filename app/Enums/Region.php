90c7d5ad7ed6bf383c20011e2d8853fec27066d4
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-03 20:55:56 +0300

    Region Enum

---
+++ app/Enums/Region.php
@@ -0,0 +1,11 @@
+<?php
+
+    namespace App\Enums;
+
+    enum Region: string {
+        case US = 'US';
+        case EU = 'EU';
+        case AU = 'AU';
+        case India = 'India';
+        case Online = 'Online';
+    }
