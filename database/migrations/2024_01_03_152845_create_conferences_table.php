f9f29ad0cc7cf17dbab97ec0220062627ee565ff
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-03 20:56:18 +0300

    Code Refactor

--- database/migrations/2024_01_03_152845_create_conferences_table.php
+++ database/migrations/2024_01_03_152845_create_conferences_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('conferences', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description');
+            $table->longText('description');
             $table->timestamp('start_date');
             $table->timestamp('end_date');
             $table->string('status');
