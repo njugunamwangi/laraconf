2c1d45ce2a0056dd4ea239a5d41a3e4aa697cdf5
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-04 13:42:37 +0300

    Add length, status and new talk to talks table

--- database/migrations/2024_01_03_152848_create_talks_table.php
+++ database/migrations/2024_01_03_152848_create_talks_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Enums\TalkLength;
+use App\Enums\TalkStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,6 +17,9 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('abstract');
+            $table->text('length')->default(TalkLength::NORMAL);
+            $table->text('status')->default(TalkStatus::SUBMITTED);
+            $table->boolean('new_talk')->default(true);
             $table->foreignId('speaker_id');
             $table->timestamps();
         });
