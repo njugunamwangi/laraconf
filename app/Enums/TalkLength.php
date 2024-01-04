4469040d3de070a52e3e1ae83c2d9ca43b1da155
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-04 17:51:25 +0300

    Code refactor

--- app/Enums/TalkLength.php
+++ app/Enums/TalkLength.php
@@ -3,7 +3,7 @@
     namespace App\Enums;
 
     enum TalkLength: string {
-        case LIGHTING = 'Lightning - 15 minutes';
+        case LIGHTNING = 'Lightning - 15 minutes';
         case NORMAL = 'Normal - 30 Minutes';
         case KEYNOTE = 'Keynote';
     }
