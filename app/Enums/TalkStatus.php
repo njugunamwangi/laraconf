0df2d914de7711bbab87415b35b00a96d9eb9da0
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-04 17:18:41 +0300

    getColor

--- app/Enums/TalkStatus.php
+++ app/Enums/TalkStatus.php
@@ -6,4 +6,12 @@
         case SUBMITTED = 'Submitted';
         case APPROVED = 'Approved';
         case REJECTED = 'Rejected';
+
+        public function getColor():string {
+            return match($this) {
+                self::SUBMITTED => 'primary',
+                self::APPROVED => 'success',
+                self::REJECTED => 'danger',
+            };
+        }
     }
