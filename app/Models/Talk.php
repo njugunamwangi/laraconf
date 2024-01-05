c7e24b832aabda2536e253e327b8b82ce1d2ee78
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-05 23:42:12 +0300

    Refactor form

--- app/Models/Talk.php
+++ app/Models/Talk.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use App\Enums\TalkLength;
 use App\Enums\TalkStatus;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -43,5 +46,23 @@ class Talk extends Model
     public function reject() {
         $this->status = TalkStatus::REJECTED;
         $this->save();
+    }
+
+    public static function getForm($speakerId = null): array {
+        return [
+            TextInput::make('title')
+                ->required()
+                ->maxLength(255),
+            RichEditor::make('abstract')
+                ->required()
+                ->maxLength(65535)
+                ->columnSpanFull(),
+            Select::make('speaker_id')
+                ->hidden(function () use ($speakerId) {
+                    return $speakerId !== null;
+                })
+                ->relationship('speaker', 'name')
+                ->required(),
+        ];
     }
 }
