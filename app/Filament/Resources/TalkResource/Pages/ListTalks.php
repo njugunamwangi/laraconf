7189b11511f43fc5d286391b5b2328da81d6356f
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-05 15:49:13 +0300

    Talk tabs

--- app/Filament/Resources/TalkResource/Pages/ListTalks.php
+++ app/Filament/Resources/TalkResource/Pages/ListTalks.php
@@ -2,9 +2,12 @@
 
 namespace App\Filament\Resources\TalkResource\Pages;
 
+use App\Enums\TalkStatus;
 use App\Filament\Resources\TalkResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListTalks extends ListRecords
 {
@@ -14,6 +17,19 @@ class ListTalks extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make(),
+            'Approved' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', TalkStatus::APPROVED)),
+            'Submitted' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', TalkStatus::SUBMITTED)),
+            'Rejected' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', TalkStatus::REJECTED)),
         ];
     }
 }
