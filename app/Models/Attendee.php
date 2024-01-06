7530a57d3b72765476350183cc651b72b91a4961
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-06 13:44:14 +0300

    Attendee form

--- app/Models/Attendee.php
+++ app/Models/Attendee.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -12,5 +14,21 @@ class Attendee extends Model
 
     public function conference(): BelongsTo {
         return $this->belongsTo(Conference::class);
+    }
+
+    public static function getForm(): array {
+        return [
+            Grid::make(2)
+                ->schema([
+                    TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('email')
+                    ->email()
+                    ->required()
+                    ->unique()
+                    ->maxLength(255)
+                ])
+        ];
     }
 }
