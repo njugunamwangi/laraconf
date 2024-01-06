223cc01c824fad2cb0c785f6879a1f8d0ff8db8a
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-06 12:35:28 +0300

    Attendee model

---
+++ app/Models/Attendee.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Attendee extends Model
+{
+    use HasFactory;
+
+    public function conference(): BelongsTo {
+        return $this->belongsTo(Conference::class);
+    }
+}
--- app/Models/Conference.php
+++ app/Models/Conference.php
@@ -22,6 +22,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Conference extends Model
 {
@@ -53,6 +54,10 @@ class Conference extends Model
     public function talks(): BelongsToMany
     {
         return $this->belongsToMany(Talk::class);
+    }
+
+    public function attendees(): HasMany {
+        return $this->hasMany(Attendee::class);
     }
 
     public static function getForm(): array {
