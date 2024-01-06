f8122da629177be685d60cb29d912f0645e1f1d4
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-06 13:44:30 +0300

    Conference sign up form

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Livewire\ConferenceSignUpPage;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/conference-sign-up', ConferenceSignUpPage::class);
 
 Route::middleware([
     'auth:sanctum',
